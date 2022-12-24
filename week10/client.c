acca9e0d29cec6063da2aaae9355b6381c002265
DungDoan <doantrungdung2001@example.com>
2022-12-24 19:51:15 +0700

    add week10

--- week10/client.c
+++ week10/client.c
@@ -1,30 +1,73 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
-void main()
-{
-    int sid;
-    char s[10]={},s1[10]={};
-    struct sockaddr_in ssock,csock;
-    sid=socket(AF_INET,SOCK_STREAM,0);
-    ssock.sin_family=AF_INET;
-    ssock.sin_addr.s_addr=inet_addr("127.0.0.1");
-    ssock.sin_port=htons(9734);
-    connect(sid,(struct sockaddr *)&ssock,sizeof(ssock));
-    while(1)
-    {
-        printf("\n Enter the string:");
-        scanf("%s",s);
-        write(sid,(void*)s,strlen(s));
-        if(strlen(s)==0)
-            break;
-        sleep(1);
-        read(sid,(void*)s1,sizeof(s1));
-        printf("\n The received string is:%s\n",s1);
-    }
-    close(sid);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 5500
+#define BUFF_SIZE 1024
+
+void send_file(FILE *fp, int sockfd);
+
+int main(int argc ,char *argv[]){
+	int client_sock,Port;
+	char buff[BUFF_SIZE + 1],*IPAdress ,*name ,*pass;
+	struct sockaddr_in server_addr; /* server's address information */
+	int msg_len, bytes_sent, bytes_received;
+	FILE *fp;
+	
+	//Step 1: Construct socket
+	client_sock = socket(AF_INET,SOCK_STREAM,0);
+	
+
+	IPAdress = argv[1];
+	Port = atoi(argv[2]);
+	name = argv[3];
+	pass = argv[4];
+	
+	//Step 2: Specify server address
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(Port);
+	server_addr.sin_addr.s_addr = inet_addr(IPAdress);
+	
+	//Step 3: Request to connect server
+	if(connect(client_sock, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) < 0){
+		printf("\nError!Can not connect to sever! Client exit imediately! ");
+		return 0;
+	}
+	
+	//Step 4: Communicate with server
+
+	//send username	
+	bytes_sent = send(client_sock, name, strlen(name), 0);
+	if(bytes_sent < 0)
+		perror("\nError: ");
+
+	//send password	
+	bytes_sent = send(client_sock, pass, strlen(pass), 0);
+	if(bytes_sent < 0)
+		perror("\nError: ");
+	
+	//received
+	bytes_received = recv(client_sock, buff, BUFF_SIZE, 0);
+	if (bytes_received < 0)
+			perror("\nError: ");
+	else if (bytes_received == 0)
+			printf("Connection closed.\n");
+		
+	buff[bytes_received] = '\0';
+	printf("Reply from server: %s", buff);
+
+	//Step 4: Close socket
+	
+	close(client_sock);
+    printf("[+]Disconnected from the server. \n");
+	return 0;
 }
--- week10/server.c
+++ week10/server.c
@@ -1,60 +1,179 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <signal.h>
 #include <unistd.h>
+#include <string.h>
+#include<ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/wait.h>
+#include <errno.h>
 
-int main()
-{
-    int server_sockfd, client_sockfd;
-    int server_len, client_len;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
+#define PORT 5500
+#define BACKLOG 20
+#define BUFF_SIZE 1024
 
-    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+typedef struct account{
+	char name[BUFF_SIZE];
+	char password[BUFF_SIZE];
+	int status;
+}account;
+account AC_STUDENT;
+typedef struct Node{
+	account data;
+	struct Node * next;
+}Node;
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(9734);
-    server_len = sizeof(server_address);
-    bind(server_sockfd, (struct sockaddr *)&server_address,server_len);
+/* Given a reference (pointer to pointer) to the head
+   of a list and an int, appends a new node at the end  */
+void append(struct Node** head_ref, account new_data){
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+  
+    struct Node *last = *head_ref;  /* used in step 5*/
+   
+    /* 2. put in the data  */
+    new_node->data  = new_data;
+  
+    /* 3. This new node is going to be the last node, so make next 
+          of it as NULL*/
+    new_node->next = NULL;
+  
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    }  
+       
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+   
+    /* 6. Change the next of last node */
+    last->next = new_node;
+    return;    
+}
 
-    /* Create a connection queue, ignore child exit details and wait for
-    clients. */
+/* Handler process signal*/
+void sig_chld(int signo);
 
-    listen(server_sockfd, 5);
+/*
+* Receive and echo message to client
+* [IN] sockfd: socket descriptor that connects to client 	
+*/
+void echo(int sockfd);
 
-    signal(SIGCHLD, SIG_IGN);
+int write_file(int sockfd);
 
-    while(1) {
-        char ch;
+int main(int argc , char *argv[]){
+	
+	int listen_sock, conn_sock; /* file descriptors */
+	struct sockaddr_in server; /* server's address information */
+	struct sockaddr_in client; /* client's address information */
+	pid_t pid;
+	int sin_size , Port;
 
-        printf("server waiting\n");
+	Node *newNode = (Node*) malloc(sizeof(Node));
+	newNode = NULL;
+	FILE *fp;
 
-        /* Accept connection. */
+	//Doc file
+	fp = fopen("taikhoan.txt","r");
+	while (feof(fp) == 0){
+		fscanf(fp,"%s",AC_STUDENT.name);
+        fgetc(fp);
+        fscanf(fp,"%s",AC_STUDENT.password);
+        fgetc(fp);
+        fscanf(fp,"%d",&AC_STUDENT.status);
+        //printf("%s %s %d \n",AC_STUDENT.name,AC_STUDENT.password,AC_STUDENT.status);
+		append(&newNode,AC_STUDENT);
+	}
+	fclose(fp);
+	/*
+	while(newNode  != NULL){
+		    printf("%-20s  %-11s   %-25d\n",newNode -> data.name, newNode->data.password, newNode->data.status);
+		    newNode = newNode->next;
 
-        client_len = sizeof(client_address);
-        client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_address, &client_len);
-
-        /* Fork to create a process for this client and perform a test to see
-        whether we're the parent or the child. */
-
-        if(fork() == 0) {
-
-        /* If we're the child, we can now read/write to the client on
-        client_sockfd.
-        The five second delay is just for this demonstration. */
-
-            read(client_sockfd, &ch, 1);
-            sleep(5);
-            ch++;
-            write(client_sockfd, &ch, 1);
-            close(client_sockfd);
-            exit(0);
-        }else {
-            close(client_sockfd); /* Otherwise, we must be the parent and our work for this client is finished. */
-        }
     }
+	*/
+	if ((listen_sock=socket(AF_INET, SOCK_STREAM, 0)) == -1 ){  /* calls socket() */
+		printf("socket() error\n");
+		return 0;
+	}
+	Port = atoi(argv[1]);
+	bzero(&server, sizeof(server));
+	server.sin_family = AF_INET;         
+	server.sin_port = htons(Port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);  /* INADDR_ANY puts your IP address automatically */   
+
+	if(bind(listen_sock, (struct sockaddr*)&server, sizeof(server))==-1){ 
+		perror("\nError: ");
+		return 0;
+	}     
+
+	if(listen(listen_sock, BACKLOG) == -1){  
+		perror("\nError: ");
+		return 0;
+	}
+	
+	/* Establish a signal handler to catch SIGCHLD */
+	signal(SIGCHLD, sig_chld);
+
+	while(1){
+		sin_size=sizeof(struct sockaddr_in);
+		if ((conn_sock = accept(listen_sock, (struct sockaddr *)&client, &sin_size))==-1){
+			if (errno == EINTR){
+				continue;
+			}
+			else{
+				perror("\nError: ");			
+				return 0;
+			}
+		}
+		
+		/* For each client, fork spawns a child, and the child handles the new client */
+		pid = fork();
+		
+		/* fork() is called in child process */
+		if(pid  == 0){
+			close(listen_sock);
+			printf("You got a connection from %s\n", inet_ntoa(client.sin_addr)); /* prints client's IP */
+			echo(conn_sock);						
+			exit(0);
+		}
+		
+		/* The parent closes the connected socket since the child handles the new client */
+		close(conn_sock);
+	}
+	close(listen_sock);
+	return 0;
+}
+
+void sig_chld(int signo){
+	pid_t pid;
+	int stat;
+	
+	/* Wait the child process terminate */
+	while((pid = waitpid(-1, &stat, WNOHANG))>0)
+		printf("\nChild %d terminated\n",pid);
+}
+
+void echo(int sockfd) {
+	char name[BUFF_SIZE],pass[BUFF_SIZE];
+	int bytes_sent, bytes_received;
+	
+	bytes_received = recv(sockfd, name, BUFF_SIZE, 0); //blocking
+	if (bytes_received < 0)
+		perror("\nError: ");
+	name[bytes_received] = '\0';
+	printf(" %s\n", name);
+	
+	bytes_sent = send(sockfd, name, bytes_received, 0); // echo to the client 
+	if (bytes_sent < 0)
+		perror("\nError: ");
+			
+	close(sockfd);
 }
